4f9c57903ac762149d7ff56fb5b330d2eec2a04e
s13265 <s13265@pjwstk.edu.pl>
2024-03-24 20:08:50 +0100

    Added FindMax method

--- Program.cs
+++ Program.cs
@@ -5,10 +5,14 @@ namespace APBD1
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             int[] numbers = { 1, 2, 3, 4, 5 };
             double average = CalculateAverage(numbers);
             Console.WriteLine($"Average: {average}");
+
+            int[] numbers2 = { 3, 7, 2, 9, 1 };
+            int max = FindMax(numbers2);
+            Console.WriteLine($"Max: {max}");
         }
 
         static double CalculateAverage(int[] numbers)
@@ -22,6 +26,20 @@ namespace APBD1
                 sum += number;
             }
             return sum / numbers.Length;
+        }
+
+        static int FindMax(int[] numbers)
+        {
+            if (numbers.Length == 0)
+                throw new InvalidOperationException("Array is empty");
+
+            int max = numbers[0];
+            foreach (var number in numbers)
+            {
+                if (number > max)
+                    max = number;
+            }
+            return max;
         }
     }
 }
